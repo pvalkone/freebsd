ce19d48d1870545f991d15a1bc65c13a3efca6cf
Petteri Valkonen <petteri.valkonen@iki.fi>
2011-05-16 20:53:15 +0300

    Send magic Wake-on-LAN packet to wake up NAS before starting backup run.

--- usr/local/bin/backup-media.bash
+++ usr/local/bin/backup-media.bash
@@ -1,6 +1,7 @@
 #!/usr/local/bin/bash -C
 
 LOCK_FILE=/tmp/backup-media.pid
+WOL_BIN=/usr/local/bin/wol
 RSYNC_BIN=/usr/local/bin/rsync
 SSH_BIN=/usr/bin/ssh
 RSYNC_LOG_FILE=${HOME}/rsync.log
@@ -10,6 +11,9 @@ REMOTE_USER=
 REMOTE_HOST=
 REMOTE_PATH=/c/media
 LOCAL_PATH=/backup
+WOL_PORT=
+WOL_MAC_ADDRESS=
+WAIT_FOR_NAS_BOOT_DELAY_IN_SECS=60
 
 if [ -e "${LOCK_FILE}" ]; then
         if pgrep -F $LOCK_FILE > /dev/null; then
@@ -25,6 +29,12 @@ fi
 trap "rm -f ${LOCK_FILE}; exit $?" INT TERM EXIT
 echo "$$" > ${LOCK_FILE}
 
-${RSYNC_BIN} -avs -P --stats --delete --log-file=${RSYNC_LOG_FILE} --exclude-from=${RSYNC_EXCLUDE_PATTERNS_FILE} -e "${SSH_BIN} -p ${REMOTE_PORT}" ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} ${LOCAL_PATH} &> /dev/null
+${WOL_BIN} -h ${REMOTE_HOST} -p ${WOL_PORT} ${WOL_MAC_ADDRESS} &> /dev/null
+# Give the NAS some time to boot up before starting backup
+sleep ${WAIT_FOR_NAS_BOOT_DELAY_IN_SECS} && \
+${RSYNC_BIN} -avs -P --stats --delete --log-file=${RSYNC_LOG_FILE} \
+--exclude-from=${RSYNC_EXCLUDE_PATTERNS_FILE} -e "${SSH_BIN} \
+-p ${REMOTE_PORT}" ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} \
+${LOCAL_PATH} &> /dev/null
 
 exit 0
