e8ec4face9ffec55ecd4f802185735a5eb70bad3
Petteri Valkonen <petteri.valkonen@iki.fi>
2012-03-08 07:15:14 +0200

    Improve error handling

--- toggle_relay.c
+++ toggle_relay.c
@@ -12,11 +12,20 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
+void print_error_and_exit(char *message) {
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
 void write_to_device(int fd, int command) {
     char sbuf[2];
     sbuf[0] = command;
-    if (write(fd, sbuf, 1) < 0) perror("Error writing output\n");
-    if (tcdrain(fd) < 0) perror("Error while waiting for output to be transmitted\n");
+    if (write(fd, sbuf, 1) < 0) {
+        print_error_and_exit("Error while writing output\n");
+    }
+    if (tcdrain(fd) < 0) {
+        print_error_and_exit("Error while waiting for output to be transmitted\n");
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -26,16 +35,21 @@ int main(int argc, char *argv[]) {
     int fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
     if (fd == -1) {
         printf("Failed to open device %s\n", device);
-        return EXIT_FAILURE;        
-    } else {
-        if (tcgetattr(fd, &defaults) < 0) perror("Failed to read port defaults\n");
-        cfmakeraw(&config);
-        if (tcsetattr(fd, TCSANOW, &config) < 0) perror("Failed to configure port\n");
-        write_to_device(fd, 0x6E); // Turn all relays off
-        sleep(3);
-        write_to_device(fd, 0x64); // Turn all relays on
-        if (tcsetattr(fd, TCSANOW, &defaults) < 0) perror("Failed to restore port defaults\n");
-        close(fd);
+        exit(EXIT_FAILURE);        
     }
+    if (tcgetattr(fd, &defaults) < 0) {
+        print_error_and_exit("Failed to read port defaults\n");
+    }
+    cfmakeraw(&config);
+    if (tcsetattr(fd, TCSANOW, &config) < 0) {
+        print_error_and_exit("Failed to configure port\n");
+    }
+    write_to_device(fd, 0x6E); // Turn all relays off
+    sleep(3);
+    write_to_device(fd, 0x64); // Turn all relays on
+    if (tcsetattr(fd, TCSANOW, &defaults) < 0) {
+        print_error_and_exit("Failed to restore port defaults\n");
+    }
+    close(fd);
     return EXIT_SUCCESS;
 }
